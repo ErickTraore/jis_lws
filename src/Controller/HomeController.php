a8b8a0891dec06feb542e68885db042546736795
root <root@vps67364.serveur-vps.net>
2020-02-14 15:48:25 +0100

    initialisation site jis

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/")
+*/
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="home_contact")
+     */
+    public function contact()
+    { 
+            return $this->redirectToRoute('/contact');
+    }
+    
+    /**
+     * @Route("/about", name="home_about")
+     */
+    public function about()
+    { 
+            return $this->render('home/about.html.twig');
+    }
+     
+}
